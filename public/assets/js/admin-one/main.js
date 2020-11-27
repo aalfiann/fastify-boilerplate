a81fd4ab1b7f5b10ae0a41dbdedbaf5abd453bee
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-11-27 20:27:12 +0700

    - update

--- public/assets/js/admin-one/main.js
+++ public/assets/js/admin-one/main.js
@@ -34,21 +34,21 @@ Array.from(document.getElementsByClassName('jb-navbar-menu-toggle')).forEach(fun
 });
 
 /* Modal: open */
-Array.from(document.getElementsByClassName('jb-modal')).forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    var modalTarget = e.currentTarget.getAttribute('data-target');
-    document.getElementById(modalTarget).classList.add('is-active');
-    document.documentElement.classList.add('is-clipped');
-  });
-});
+// Array.from(document.getElementsByClassName('jb-modal')).forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     var modalTarget = e.currentTarget.getAttribute('data-target');
+//     document.getElementById(modalTarget).classList.add('is-active');
+//     document.documentElement.classList.add('is-clipped');
+//   });
+// });
 
 /* Modal: close */
-Array.from(document.getElementsByClassName('jb-modal-close')).forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.currentTarget.closest('.modal').classList.remove('is-active');
-    document.documentElement.classList.remove('is-clipped');
-  });
-});
+// Array.from(document.getElementsByClassName('jb-modal-close')).forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.currentTarget.closest('.modal').classList.remove('is-active');
+//     document.documentElement.classList.remove('is-clipped');
+//   });
+// });
 
 /* Notification dismiss */
 Array.from(document.getElementsByClassName('jb-notification-dismiss')).forEach(function (el) {
